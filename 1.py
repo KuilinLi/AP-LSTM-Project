3861be4215bcf8ee3ea0df9c635664e69f1fb90b
anonymous2021-123456 <81853881+anonymous2021-123456@users.noreply.github.com>
2021-04-21 21:08:20 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,33 +1,207 @@
+from typing import Tuple
 import torch
 import torch.nn as nn
+from torch.nn import Parameter
+from torch.nn import init
+from torch import Tensor
+import torch.nn.functional as F
+import math
 
-class BiLSTM_Attention(nn.Module):
-    def __init__(self):
-        super(BiLSTM_Attention, self).__init__()
 
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, n_hidden, bidirectional=True)
-        self.out = nn.Linear(n_hidden * 2, num_classes)
+class PhyaLSTM(nn.Module):
+    """Implementation of the physics-aware-LSTM (PhyaLSTM)
+        Parameters
+        ----------
+        input_size : int
+            Number of dynamic features, which are those, passed to the LSTM at each time step---x_t
+        Sca_size : int
+            Number of static catchment attributes---Sca(t)
+        hidden_size : int
+            Number of hidden/memory cells.
+        """
+    def __init__(self, input_size: int, Sca_size: int, hidden_size: int, with_AM: bool=False):
+        super(PhyaLSTM, self).__init__()
+        self.input_size = input_size
+        self.Sca_size = Sca_size
+        self.hidden_size = hidden_size
+        self.ME = True  # Memory Enhancement
+        self.with_am = with_AM
 
-    # lstm_output : [batch_size, n_step, n_hidden * num_directions(=2)], F matrix
-    def attention_net(self, lstm_output, final_state):
-        hidden = final_state.view(-1, n_hidden * 2, 1)   # hidden : [batch_size, n_hidden * num_directions(=2), 1(=n_layer)]
-        attn_weights = torch.bmm(lstm_output, hidden).squeeze(2) # attn_weights : [batch_size, n_step]
-        soft_attn_weights = F.softmax(attn_weights, 1)
-        # [batch_size, n_hidden * num_directions(=2), n_step] * [batch_size, n_step, 1] = [batch_size, n_hidden * num_directions(=2), 1]
-        context = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
-        return context, soft_attn_weights.data.numpy() # context : [batch_size, n_hidden * num_directions(=2)]
+        # input gate
+        self.w_ix = Parameter(Tensor(hidden_size, input_size))
+        self.w_ih = Parameter(Tensor(hidden_size, hidden_size))
+        self.w_ia = Parameter(Tensor(hidden_size, hidden_size))
+        self.b_i = Parameter(Tensor(hidden_size, 1))
 
-    def forward(self, X):
-        input = self.embedding(X) # input : [batch_size, len_seq, embedding_dim]
-        input = input.permute(1, 0, 2) # input : [len_seq, batch_size, embedding_dim]
+        # forget gate
+        self.w_fx = Parameter(Tensor(hidden_size, input_size))
+        self.w_fo = Parameter(Tensor(hidden_size, hidden_size))
+        self.w_fa = Parameter(Tensor(hidden_size, hidden_size))
+        self.b_f = Parameter(Tensor(hidden_size, 1))
 
-        hidden_state = Variable(torch.zeros(1*2, len(X), n_hidden)) # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
-        cell_state = Variable(torch.zeros(1*2, len(X), n_hidden)) # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        # output gate
+        self.w_ox = Parameter(Tensor(hidden_size, input_size))
+        self.w_oh = Parameter(Tensor(hidden_size, hidden_size))
+        self.w_oa = Parameter(Tensor(hidden_size, hidden_size))
+        self.b_o = Parameter(Tensor(hidden_size, 1))
 
-        # final_hidden_state, final_cell_state : [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
-        output, (final_hidden_state, final_cell_state) = self.lstm(input, (hidden_state, cell_state))
-        output = output.permute(1, 0, 2) # output : [batch_size, len_seq, n_hidden]
-        attn_output, attention = self.attention_net(output, final_hidden_state)
-        return self.out(attn_output), attention # model : [batch_size, num_classes], attention : [batch_size, n_step]
+        # cell
+        self.w_gx = Parameter(Tensor(hidden_size, input_size))
+        self.w_gh = Parameter(Tensor(hidden_size, hidden_size))
+        self.b_g = Parameter(Tensor(hidden_size, 1))
+
+        # sca(t)
+        self.w_a = Parameter(Tensor(hidden_size, Sca_size))
+        self.b_a = Parameter(Tensor(hidden_size, 1))
+
+        # assimilation obs(AM)
+        if self.with_am:
+            self.w_sim = Parameter(Tensor(hidden_size, hidden_size))
+            self.w_obs = Parameter(Tensor(hidden_size, hidden_size))
+            self.b_k = Parameter(Tensor(hidden_size, 1))
+
+        self.reset_weigths()
+
+    def reset_weigths(self):
+        """reset weights
+        """
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            init.uniform_(weight, -stdv, stdv)
+
+    def calc_corr(self, a, b):
+        """Calculate the correlation coefficient of two vectors: a, b
+        """
+        a_avg = sum(a) / len(a)
+        b_avg = sum(b) / len(b)
+        # Calculate the molecule, covariance --- according to the covariance formula,
+        # it should be divided by n, because it reduces n up and down in the correlation coefficient, so it can not be divided by n.
+        cov_ab = sum([(x - a_avg) * (y - b_avg) for x, y in zip(a, b)])
+        # Calculate the denominator, variance product --- variance should be divided by N, so it can not be divided by n.
+        sq = math.sqrt(sum([(x - a_avg) ** 2 for x in a]) * sum([(x - b_avg) ** 2 for x in b]))
+        corr_factor = cov_ab / sq
+        return corr_factor
+
+
+    def forward(self,
+                x_input: Tensor,
+                x_sca: Tensor,
+                soil_state: Tensor,
+                h_obs: Tensor=None,
+                h_input:Tensor=None,
+                c_input:Tensor=None) -> Tuple[Tensor, Tensor]:
+        """forward,
+        Args:
+            inputs: [batch_size, seq_size, input_size]
+            input_Sca: [batch_size, seq_size, Sca_size]
+            soil_state: [batch_size, seq_size]
+            h_obs: [batch_size, seq_size, hidden_size]
+        """
+        batch_size, seq_size, input_dim = x_input.size()
+
+        h_output = torch.zeros(batch_size, seq_size, self.hidden_size)
+        c_output = torch.zeros(batch_size, seq_size, self.hidden_size)
+
+        if h_input==None:
+            h_t = torch.zeros(batch_size, self.hidden_size).t()
+        if c_input==None:
+            c_t = torch.zeros(batch_size, self.hidden_size).t()
+
+        r = torch.zeros(batch_size, self.hidden_size)  # correlation coefficient
+
+        for t in range(seq_size):
+            x_t = x_input[:, t, :].t()    # [input_size, batch_size]
+            sca_t = x_sca[:, t, :].t()    # [Sca_size, batch_size]
+
+            a_t = torch.sigmoid(self.w_a @ sca_t + self.b_a)
+            # input gate
+            i = torch.sigmoid(self.w_ix @ x_t + self.w_ih @ h_t + self.w_ia @ a_t + self.b_i)
+            # cell
+            g = torch.tanh(self.w_gx @ x_t + self.w_gh @ h_t + self.b_g)
+            # forget gate
+            f = torch.sigmoid(self.w_fx @ x_t + self.w_fo @ h_t + self.w_fa @ a_t + self.b_f)
+            # output gate
+            o = torch.sigmoid(self.w_ox @ x_t + self.w_oh @ h_t + self.w_oa @ a_t + self.b_o)
+
+            c_next = f * c_t + i * g    # [hidden_dim, batch_size]
+            h_next = o * torch.tanh(c_next)   # [hidden_dim, batch_size]
+
+            if self.ME:
+                for i in range(batch_size):
+                    r[i, :] = self.calc_corr(c_output[i, :t], soil_state[i, :t])
+                c_next = c_next.mul(abs(r)+1)
+
+            if self.with_am:
+                K = torch.tanh(self.w_sim @ h_next + self.w_obs @ h_obs + self.b_k)
+                h_next = h_next + K.mul(h_obs - h_next)
+
+            h_output[:, t] = h_next.t()  # transpose:[batch_size, hidden_dim]
+            c_output[:, t] = c_next.t()
+
+            h_t = h_next
+            c_t = c_next
+
+        return (h_output, c_output)
+
+
+
+class Obs_encoder(nn.Module):
+    def __init__(self, x_obs_size, sca_size, hidden_size):
+        super(Obs_encoder, self).__init__()
+        self.x_obs_size = x_obs_size
+        self.hidden_size = hidden_size
+        self.sca_size = sca_size
+
+        self.obs_encoder = PhyaLSTM(x_obs_size, hidden_size, sca_size, with_AM=False)
+
+    def forward(self, x_obs, x_sca, soil_state:Tensor):
+        h_obs, _ = self.obs_encoder(x_obs, x_sca, soil_state, None)
+        return h_obs
+
+
+
+
+class Prediction_module(nn.Module):
+    def __init__(self, input_size, sca_size, x_obs_size, hidden_size, window_size):
+        super(Prediction_module, self).__init__()
+        self.x_size = input_size
+        self.sca_size = sca_size
+        self.x_obs_size = x_obs_size
+        self.hidden_size = hidden_size
+        self.window_size = window_size
+
+        self.obs_encoder = PhyaLSTM(x_obs_size, sca_size, hidden_size, with_AM=False)  # x_obs_size == input_size
+        self.prediction = PhyaLSTM(input_size, sca_size, hidden_size, with_AM=True)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(in_features=hidden_size, out_features=1)
+        self.w_omega = Parameter(Tensor(input_size, input_size))
+
+
+    def forward(self, x_input:Tensor,
+                x_sca:Tensor,
+                soil_state:Tensor,
+                x_obs:Tensor):
+
+        h_obs, _ = self.obs_encoder(x_obs, x_sca, soil_state)
+        h_, c_ = self.prediction(x_input[:,:-1], x_sca[:,:-1], soil_state[:,:-1], h_obs=h_obs[:,:-1])
+        h_t = h_[:,-1:,:]
+
+        # Attention Net
+        x_i = x_input[:, -self.window_size:, :]   # [batch_size, window_size, input_size]
+        x_t = x_input[:, -1:, :].transpose(1,2)                # [batch_size, input_size, 1]
+        u = torch.tanh(torch.matmul(x_i, self.w_omega))  # [batch_size, window_size, input_size]
+        att = torch.matmul(u, x_t)      # [batch_size, window_size, 1]
+        att_score = F.softmax(att, dim=1)
+
+        scored_c = c_[:, -self.window_size:, :] * att_score  # [batch_size, window_size, hidden_size]
+        c_t = torch.sum(scored_c, dim=1)    # [batch_size, 1, hidden_size]
+
+        h_t, _ = self.prediction(x_input[:,-1:], x_sca[:,-1:], soil_state[:,-1:], h_obs[:,-1:], h_input=h_t, c_input=c_t)
+
+        pred = self.fc1(self.relu(h_t))   # [batch_size, 1, 1]
+        pred = pred.squeeze(2)  # [batch_size, 1]
+
+        return pred
+
+
 
